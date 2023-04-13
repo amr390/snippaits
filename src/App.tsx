0683a7568c3e609b91370fdfb07aa1c4477ca2c2
amr <a.mas.rodriguez@gmail.com>
2023-04-13 08:01:41 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-red-500"> red text </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
