0adf747b6ebfe5cf9d908e830dd15ef38c21c298
amr <a.mas.rodriguez@gmail.com>
2023-04-15 15:09:35 +0200

    ---

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,27 +1,17 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../styles/App.css';
+import React from 'react'
+import '../styles/App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500"> red text </p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className='flex flex-1 flex-row justify-between'>
+        <span>burger button</span>
+        <span>Login</span>
+      </nav>
+      <div className=''></div>
+      <div className=''></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
